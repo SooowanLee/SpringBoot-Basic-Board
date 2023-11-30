cad79548312f0cf9a77986b7afa7a0dbb37cd269
Soowan Lee <lsy9406@gmail.com>
2023-11-30 18:06:50 +0900

    add Init Git Project

---
+++ src/main/java/com/study/basicboard/BasicboardApplication.java
@@ -0,0 +1,13 @@
+package com.study.basicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicboardApplication.class, args);
+    }
+
+}
