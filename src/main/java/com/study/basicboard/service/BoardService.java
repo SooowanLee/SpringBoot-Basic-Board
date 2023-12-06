83294726ffed1c6827b70deeca8d0936742b4118
Soowan Lee <lsy9406@gmail.com>
2023-12-06 18:16:37 +0900

    add 게시글 조회, 삭제, 수정 등 비즈니스 로직

---
+++ src/main/java/com/study/basicboard/service/BoardService.java
@@ -0,0 +1,166 @@
+package com.study.basicboard.service;
+
+import com.study.basicboard.domain.dto.BoardCntDto;
+import com.study.basicboard.domain.dto.BoardCreateRequest;
+import com.study.basicboard.domain.dto.BoardDto;
+import com.study.basicboard.domain.entity.*;
+import com.study.basicboard.domain.enum_class.BoardCategory;
+import com.study.basicboard.domain.enum_class.UserRole;
+import com.study.basicboard.repository.BoardRepository;
+import com.study.basicboard.repository.CommentRepository;
+import com.study.basicboard.repository.LikeRepository;
+import com.study.basicboard.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+    private final LikeRepository likeRepository;
+    private final CommentRepository commentRepository;
+    private final UploadImageService uploadImageService;
+
+    public Page<Board> getBoardList(BoardCategory category, PageRequest pageRequest, String searchType, String keyword) {
+        if (searchType != null && keyword != null) {
+            if (searchType.equals("title")) {
+                return boardRepository.findAllByCategoryAndTitleContainsAndUserUserRoleNot(category, keyword, UserRole.ADMIN, pageRequest);
+            } else {
+                return boardRepository.findAllByCategoryAndUserNicknameContainsAndUserUserRoleNot(category, keyword, UserRole.ADMIN, pageRequest);
+            }
+        }
+        return boardRepository.findAllByCategoryAndUserUserRoleNot(category, UserRole.ADMIN, pageRequest);
+    }
+
+    public List<Board> getNotice(BoardCategory category) {
+        return boardRepository.findAllByCategoryAndUserUserRole(category, UserRole.ADMIN);
+    }
+
+    public BoardDto getBoard(Long boardId, String category) {
+        Optional<Board> optBoard = boardRepository.findById(boardId);
+
+        //id에 해당하는 게시글이 없거나 카테고리가 일치하지 않으면 null return
+        if (optBoard.isEmpty() || !optBoard.get().getCategory().toString().equalsIgnoreCase(category)) {
+            return null;
+        }
+
+        return BoardDto.of(optBoard.get());
+    }
+
+    @Transactional
+    public Long writeBoard(BoardCreateRequest req, BoardCategory category, String loginId, Authentication auth) throws IOException {
+        User loginUser = userRepository.findByLoginId(loginId).get();
+
+        Board savedBoard = boardRepository.save(req.toEntity(category, loginUser));
+
+        UploadImage uploadImage = uploadImageService.saveImage(req.getUploadImage(), savedBoard);
+        if (uploadImage != null) {
+            savedBoard.setUploadImage(uploadImage);
+        }
+
+        if (category.equals(BoardCategory.GREETING)) {
+            loginUser.rankUp(UserRole.SILVER, auth);
+        }
+        return savedBoard.getId();
+    }
+
+    @Transactional
+    public Long editBoard(Long boardId, String category, BoardDto dto) throws IOException {
+        Optional<Board> optBoard = boardRepository.findById(boardId);
+
+        //id에 해당하는 게시글이 없거나 카테고리가 일치하지 않으면 null return
+        if (optBoard.isEmpty() || !optBoard.get().getCategory().toString().equalsIgnoreCase(category)) {
+            return null;
+        }
+
+        Board board = optBoard.get();
+        //게시글 이미지가 있었으면 삭제
+        if (board.getUploadImage() != null) {
+            uploadImageService.deleteImage(board.getUploadImage());
+            board.setUploadImage(null);
+        }
+
+        UploadImage uploadImage = uploadImageService.saveImage(dto.getNewImage(), board);
+        if (uploadImage != null) {
+            board.setUploadImage(uploadImage);
+        }
+        board.update(dto);
+
+        return board.getId();
+    }
+
+    public Long deleteBoard(Long boardId, String category) throws IOException {
+        Optional<Board> optBoard = boardRepository.findById(boardId);
+
+        //id에 해당하는 게시글이 없거나 카테고리가 일치하지 않으면 null return
+        if (optBoard.isEmpty() || !optBoard.get().getCategory().toString().equalsIgnoreCase(category)) {
+            return null;
+        }
+
+        /**
+         * 1. 해당 게시물의 작성자 정보를 가져옴
+         * 2. 작성자가 받은 좋아요 수에서 해당 게시물의 좋아요 수를 차감
+         * 3. 게시물에 업로드된 이미지가 있다면 이미지를 삭제
+         */
+        User boardUser = optBoard.get().getUser();
+        boardUser.likeChange(boardUser.getReceivedLikeCnt() - optBoard.get().getLikeCnt());
+        if (optBoard.get().getUser() != null) {
+            uploadImageService.deleteImage(optBoard.get().getUploadImage());
+        }
+        boardRepository.deleteById(boardId);
+        return boardId;
+    }
+
+    public String getCategory(Long boardId) {
+        Board board = boardRepository.findById(boardId).get();
+        return board.getCategory().toString().toLowerCase();
+    }
+
+    public List<Board> findMyBoard(String category, String loginId) {
+        if (category.equals("board")) {
+            return boardRepository.findAllByUserLoginId(loginId);
+        } else if (category.equals("like")) {
+            List<Like> likes = likeRepository.findAllByUserLoginId(loginId);
+            List<Board> boards = new ArrayList<>();
+            for (Like like : likes) {
+                boards.add(like.getBoard());
+            }
+            return boards;
+        } else if (category.equals("comment")) {
+            List<Comment> comments = commentRepository.findAllByUserLoginId(loginId);
+            List<Board> boards = new ArrayList<>();
+            HashSet<Long> commentIds = new HashSet<>();
+
+            for (Comment comment : comments) {
+                if (!commentIds.contains(comment.getBoard().getId())) {
+                    boards.add(comment.getBoard());
+                    commentIds.add(comment.getBoard().getId());
+                }
+            }
+            return boards;
+        }
+        return null;
+    }
+
+    public BoardCntDto getBoardCnt() {
+        return BoardCntDto.builder()
+                .totalBoardCnt(boardRepository.count())
+                .totalNoticeCnt(boardRepository.countAllByUserUserRole(UserRole.ADMIN))
+                .totalGreetingCnt(boardRepository.countAllByCategoryAndUserUserRoleNot(BoardCategory.GREETING, UserRole.ADMIN))
+                .totalFreeCnt(boardRepository.countAllByCategoryAndUserUserRoleNot(BoardCategory.FREE, UserRole.ADMIN))
+                .totalGoldCnt(boardRepository.countAllByCategoryAndUserUserRoleNot(BoardCategory.GOLD, UserRole.ADMIN))
+                .build();
+    }
+}
