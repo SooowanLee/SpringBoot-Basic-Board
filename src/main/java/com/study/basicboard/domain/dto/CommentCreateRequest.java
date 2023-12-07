9c5bc020440d39dada09e24c3efc62247549e511
Soowan Lee <lsy9406@gmail.com>
2023-12-07 23:34:14 +0900

    add 댓글 생성 DTO

--- src/main/java/com/study/basicboard/domain/dto/CommentCreateRequest.java
+++ src/main/java/com/study/basicboard/domain/dto/CommentCreateRequest.java
@@ -3,7 +3,9 @@ package com.study.basicboard.domain.dto;
 import com.study.basicboard.domain.entity.Board;
 import com.study.basicboard.domain.entity.Comment;
 import com.study.basicboard.domain.entity.User;
+import lombok.Data;
 
+@Data
 public class CommentCreateRequest {
 
     private String body;
