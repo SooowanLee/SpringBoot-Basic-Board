f7010173bec99e13aa0b9f6cdc98f162e6f24306
Soowan Lee <lsy9406@gmail.com>
2023-12-07 23:39:02 +0900

    add 좋아요 Repository

---
+++ src/main/java/com/study/basicboard/repository/LikeRepository.java
@@ -0,0 +1,16 @@
+package com.study.basicboard.repository;
+
+import com.study.basicboard.domain.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    void deleteByUserLoginIdAndBoardId(String loginId, Long boardId);
+    Boolean existsByUserLoginIdAndBoardId(String loginId, Long boardId);
+
+    List<Like> findAllbyUserLoginId(String loginId);
+}
