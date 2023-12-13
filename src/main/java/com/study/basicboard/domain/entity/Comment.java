c1c6ab0b0b0aff82e9d8962647a40a2098a617bd
Soowan Lee <lsy9406@gmail.com>
2023-12-13 17:16:31 +0900

    add 댓글 생성시간 및 수정시간을 표시하기 위해 BaseEntity 상속

--- src/main/java/com/study/basicboard/domain/entity/Comment.java
+++ src/main/java/com/study/basicboard/domain/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.study.basicboard.domain.entity;
 
+import com.study.basicboard.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Getter
-public class Comment {
+public class Comment extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
